0b53bc4619815e9dbeace8f5cad0ab4c930a55d4
Walid Wazini <wazini25@gmail.com>
2021-10-14 23:44:51 +0800

    first animated text

--- lib/screens/screen_welcome.dart
+++ lib/screens/screen_welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatelessWidget {
   //const WelcomeScreen({Key? key}) : super(key: key);
@@ -12,9 +13,22 @@ class WelcomeScreen extends StatelessWidget {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('')
+            image: AssetImage(''),
+            fit: BoxFit.cover,
           )
         ),
+        child: Column(children: [
+          Container(
+            child: AnimatedTextKit(
+              animatedTexts: [
+                ColorizeAnimatedText('Quiz Game',
+                    textStyle: TextStyle(),
+                    colors:[Colors.red, Colors.deepPurple]
+                ),
+              ],
+            ),
+          )
+        ],),
       ),
     );
   }
