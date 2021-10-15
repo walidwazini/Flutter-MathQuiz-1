2587d1e567d69bb8be2fa3afe3387c57ac8a75e2
Walid Wazini <wazini25@gmail.com>
2021-10-16 00:48:52 +0800

    highscore

--- lib/screens/screen_game.dart
+++ lib/screens/screen_game.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
-class GameScreen extends StatelessWidget {
+class GameScreen extends StatefulWidget {
   //const GameScreen({Key? key}) : super(key: key);
   static final id = 'game_screen';
 
+  @override
+  _GameScreenState createState() => _GameScreenState();
+}
 
+class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        color: Colors.lightGreenAccent,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xff1542bf), Colors.lightBlueAccent],
+          )
+        ),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+          Text('HighScore',  style: TextStyle(fontSize: 30, color: Colors.yellowAccent),),
+          SizedBox(height: 20,),
+          Text('0', style: TextStyle(fontSize: 30, color: Colors.black),)
+        ],),
       ),
     );
   }
